e786ebd91985c5e50b8bf08c6488e1eb14597756
amanda <amanda.m.kunz@gmail.com>
2020-01-13 18:59:17 -0600

    fixed duplicate cards on task.js added DB logic to create and save cards, added columns for cards

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,9 @@
+// import { TableHints } from "sequelize/types";
+
 var token = window.localStorage.getItem("Authorization");
 
 var jwtToken = "Bearer " + token;
-console.log(token);
+console.log(token); //commented out jwt logic temporarily due to login page css messed up
 if(!token){
   window.location.pathname = "/login"; //if no token redirect to login
 }
@@ -11,46 +13,64 @@ $.ajax({
   method: "POST",
   headers: {authorization: jwtToken}
 }).then(function(response) {
-
-  console.log(response);
+  var task = response;
+  console.log(task);
+  for(var i = 0; i < task.length; i++){
+    var taskObj = {
+      name: task[i].task_title,
+      description: task[i].task_text,
+      id: task[i].task_id
+    }
+    addCols(taskObj);
+  }
 });
 
-$(".dragbox ul").sortable({
-  connectWith: "ul",
-  items: "li:not(.drag-disabled)",
+$(".dragbox").sortable({
+  connectWith: ".drag-column",
+  items: ".draggable",
   dropOnEmpty: false
 });
+
+function addCols(taskObj) {
+  console.log("adding columns");
+  var myCol = $(`<div class="col-sm-3 col-md-3 pb-2" value="${taskObj.id}"></div>`);
+  var myPanel = $(
+    `<div class="card card-outline-info ui-state-default draggable" id="Panel"><div class="card-block"><div class="card-title"><span> ${taskObj.name}</span><button type="button" class="close" data-target="#Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p>${taskObj.description}</p></div></div>`
+  );
+  myPanel.appendTo(myCol);
+  myCol.appendTo(".to-do");
+}
 
 // $(".clickme").on("click", function(event){
 //   event.preventDefault();
 //   alert("I'm clickable!")
 // });
 
-$(function(){
-  $('.clickme').popover({
+// $(function(){
+//   $('.clickme').popover({
      
-      placement: 'bottom',
-      title: 'Enter a Task',
-      sanitize: false,
-      html:true,
-      content:  `
-      <div id="pops">
-        <div>
-            <label for="Task">Task:</label>
-            <input type="text" name="task" id="pop-task" class="form-control input-md">
-            <button type="button" id="pop-button" class="btn btn-primary" data-loading-text="Sending info.."><em class="icon-ok"></em> Save</button>
-        </div>
-    </div>
-      `
-  }).on('click', function(){
-    // had to put it within the on click action so it grabs the correct info on submit
-    $(this).addClass("selected");
-    console.log($(this));
-    $('#pop-button').click(function(){
-        var newTask = $("#pop-task").val().trim();
-        $(".selected").text(newTask);
-        $("li").removeClass("selected");
-        $('.clickme').popover('hide');
-    });
-})
-})
+//       placement: 'bottom',
+//       title: 'Enter a Task',
+//       sanitize: false,
+//       html:true,
+//       content:  `
+//       <div id="pops">
+//         <div>
+//             <label for="Task">Task:</label>
+//             <input type="text" name="task" id="pop-task" class="form-control input-md">
+//             <button type="button" id="pop-button" class="btn btn-primary" data-loading-text="Sending info.."><em class="icon-ok"></em> Save</button>
+//         </div>
+//     </div>
+//       `
+//   }).on('click', function(){
+//     // had to put it within the on click action so it grabs the correct info on submit
+//     $(this).addClass("selected");
+//     console.log($(this));
+//     $('#pop-button').click(function(){
+//         var newTask = $("#pop-task").val().trim();
+//         $(".selected").text(newTask);
+//         $("li").removeClass("selected");
+//         $('.clickme').popover('hide');
+//     });
+// })
+// })
--- public/js/task.js
+++ public/js/task.js
@@ -1,3 +1,7 @@
+var token = window.localStorage.getItem("Authorization");
+
+var jwtToken = "Bearer " + token;
+// eslint-disable-next-line no-unused-vars
 function searchToggle(obj, evt) {
   var container = $(obj).closest(".search-wrapper");
   if (!container.hasClass("active")) {
@@ -14,46 +18,64 @@ function searchToggle(obj, evt) {
     container.find(".searcharea").val("");
   }
 }
-$(".textarea").hide();
-$(".search-icon").on("click", function() {
-  $(this).attr("id", "open");
-  $(".textarea").show("slow", "swing");
 
+$(".search-icon").on("click", function(e) {
+  e.preventDefault();
+  $(this).attr("id", "open");
+  $(".show-on-click").show();
+  $(".searcharea").show("slow", "swing");
+  console.log($(this));
+});
+
+$("#save-task").on("click", function(e){
+  e.preventDefault();
   var task = {
     name: $(".search-input")
       .val()
       .trim(),
     description: $(".searcharea")
       .val()
-      .trim()
+      .trim(),
+    status: "to-do",
+    id: ""
   };
+  console.log("I've been clicked");
   if (task.name.length > 0 && task.description.length > 0) {
-    console.log(task);
-    function addCols() {
-      var myCol = $('<div class="col-sm-3 col-md-3 pb-2"></div>');
-      var myPanel = $(
-        `<div class="card card-outline-info" id="Panel"><div class="card-block"><div class="card-title"><span> ${task.name}</span><button type="button" class="close" data-target="#Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p>${task.description}</p></div></div>`
-      );
-      myPanel.appendTo(myCol);
-      myCol.appendTo("#contentPanel");
-
-      $(".close").on("click", function(e) {
-        e.stopPropagation();
-        var $target = $(this).parents(".col-sm-3");
-        $target.hide("slow", function() {
-          $target.remove();
-        });
-      });
-    }
-    addCols();
+    console.log(task);    
+    $(".textarea").hide();
     $.ajax({
-      url: "/dashboard",
+      url: "/api/createtask",
       method: "POST",
+      headers: {authorization: jwtToken},
       data: task
-    }).then(function(response) {});
+    }).then(function(response) {
+      task.id = response.task_id;
+      addCols(task);
+      console.log(task.id);
+    });
   }
 });
 $(".close").on("click", function() {
   $(".search-icon").removeAttr("id");
   $(".textarea").hide("slow", "swing");
+});
+
+
+function addCols(task) {
+  console.log("adding columns");
+  var myCol = $(`<div class="col-sm-3 col-md-3 pb-2" value="${task.id}"></div>`);
+  var myPanel = $(
+    `<div class="card card-outline-info ui-state-default draggable" id="Panel"><div class="card-block"><div class="card-title"><span> ${task.name}</span><button type="button" class="close" data-target="#Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p>${task.description}</p></div></div>`
+  );
+  myPanel.appendTo(myCol);
+  myCol.appendTo(".to-do");
+}
+
+$(".close").on("click", function(e) {
+  console.log(this);
+  e.stopPropagation();
+  var $target = $(this).parents(".col-sm-3");
+  $target.hide("slow", function() {
+    $target.remove();
+  });
 });
