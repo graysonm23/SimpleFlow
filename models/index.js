edee48615db1150cd0fd4bbf0e5b48d20def69fa
Grayson M <grayson.mcmurry23@gmail.com>
2020-01-14 18:55:41 -0600

    adding

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var mysql = require("mysql");
 var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
+var db = require("./");
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
